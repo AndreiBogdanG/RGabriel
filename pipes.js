7b00ebef48f0a4d46fed3815028c0293b135cc8e
AndreiBogdanG <alcatraz0405@yahoo.com>
2025-10-08 19:06:58 +0300

    secondary pictures

--- pipes.js
+++ pipes.js
@@ -13,7 +13,7 @@ const mesajEroare = {
 const pipe1 = {
   item: "1",
   image: "Gallery/Acartar/Acartar.jpg",
-  image1: "",
+  image1: "Gallery/Acartar/Acartar1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -32,7 +32,7 @@ const pipe1 = {
 const pipe2 = {
   item: "2",
   image: "Gallery/Cavour/Cavour.jpg",
-  image1: "",
+  image1: "Gallery/Cavour/Cavour1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -51,7 +51,7 @@ const pipe2 = {
 const pipe3 = {
   item: "3",
   image: "Gallery/Corso/Corso.jpg",
-  image1: "",
+  image1: "Gallery/Corso/Corso1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -70,7 +70,7 @@ const pipe3 = {
 const pipe4 = {
   item: "4",
   image: "Gallery/Dante/Dante.jpg",
-  image1: "",
+  image1: "Gallery/Dante/Dante1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -89,7 +89,7 @@ const pipe4 = {
 const pipe5 = {
   item: "5",
   image: "Gallery/Fortis/Fortis.jpg",
-  image1: "",
+  image1: "Gallery/Fortis/Fortis1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -108,7 +108,7 @@ const pipe5 = {
 const pipe6 = {
   item: "6",
   image: "Gallery/Lido/Lido.jpg",
-  image1: "",
+  image1: "Gallery/Lido/Lido1.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -127,7 +127,7 @@ const pipe6 = {
 const pipe7 = {
   item: "7",
   image: "Gallery/Notte/Notte.jpg",
-  image1: "",
+  image1: "Gallery/Notte/Notte.jpg",
   image2: "",
   image3: "",
   image4: "",
@@ -146,7 +146,7 @@ const pipe7 = {
 const pipe8 = {
   item: "8",
   image: "Gallery/Vetra/Vetra.jpg",
-  image1: "",
+  image1: "Gallery/Vetra/Vetra1.jpg",
   image2: "",
   image3: "",
   image4: "",
